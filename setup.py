f37eb9b4c6d4b510113660a2af29615cf381f94e
gambrivius <claponsie@gmail.com>
2020-08-30 23:34:07 -0400

    fixing setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='mmJournal',
-    packages=['mmJournal'],
     include_package_data=True,
     install_requires=[
         'flask',
         'flask-login',
+        'passlib',
     ],
 )
